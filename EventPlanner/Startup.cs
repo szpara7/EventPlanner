7c7f5797a60a28a9bfa14db8d03ac0347e85d7cb
PS\Przemek <przemek.szpara@gmail.com>
2017-12-19 18:02:37 +0100

    Add project files.

---
+++ EventPlanner/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventPlanner.Startup))]
+namespace EventPlanner
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
