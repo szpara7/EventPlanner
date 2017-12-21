3413cd37150fe28154e7496aefa6ecb3b53f7de8
PS\Przemek <przemek.szpara@gmail.com>
2017-12-21 11:40:15 +0100

    add DependencyInjection and register it in Global.asax.cs

--- EventPlanner/Global.asax.cs
+++ EventPlanner/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EventPlanner.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,7 @@ namespace EventPlanner
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DependencyResolver.SetResolver(new DependencyInjectionController());
         }
     }
 }
---
+++ EventPlanner/Infrastructure/DependencyInjectionController.cs
@@ -0,0 +1,39 @@
+﻿using Ninject;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EventPlanner.Infrastructure
+{
+    public class DependencyInjectionController : IDependencyResolver
+    {
+        private IKernel ninjectKernel;
+
+        public DependencyInjectionController()
+        {
+            this.ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+
+        public object GetService(Type serviceType)
+        {
+            return ninjectKernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return ninjectKernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+
+        }
+    }
+
+}
