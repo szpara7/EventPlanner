1ae1449be7a731a5833d3d0449e25f401474fa34
KOMP\Przemek <przemek.szpara@gmail.com>
2018-02-20 23:11:53 +0100

    implemented EmailService

--- EventPlanner/App_Start/IdentityConfig.cs
+++ EventPlanner/App_Start/IdentityConfig.cs
@@ -13,6 +13,9 @@ using Microsoft.Owin.Security;
 using EventPlanner.Models;
 using EventPlanner.Common.Models;
 using EventPlanner.Common;
+using MimeKit;
+using MailKit.Net.Smtp;
+using System.Configuration;
 
 namespace EventPlanner
 {
@@ -21,7 +24,25 @@ namespace EventPlanner
         public Task SendAsync(IdentityMessage message)
         {
             // Plug in your email service here to send an email.
-            return Task.FromResult(0);
+            var mailMessage = new MimeMessage();
+            mailMessage.From.Add(new MailboxAddress("Event Planner", "eventplanner@interia.pl"));
+            mailMessage.To.Add(new MailboxAddress(message.Destination));
+            mailMessage.Subject = message.Subject;
+            mailMessage.Body = new TextPart()
+            {
+                Text = message.Body
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                client.Connect("poczta.interia.pl", 465);
+                client.Authenticate(ConfigurationManager.AppSettings["mailUser"], ConfigurationManager.AppSettings["mailPassword"]);
+                client.Send(mailMessage);
+                client.Disconnect(true);
+            }
+
+               return Task.FromResult(0);
         }
     }
 
